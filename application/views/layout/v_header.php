d59e8a05d7788a058c88fb5020eff630c50f913e
Denny Andreas <64694913+dennyandreas@users.noreply.github.com>
2022-06-21 14:51:34 +0700

    Initial commit

---
+++ application/views/layout/v_header.php
@@ -0,0 +1,14 @@
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <div class="container">
+      <a href="<?php echo base_url('home'); ?> " class="navbar-brand">
+        <img src="<?php echo base_url() ?>assets/dist/img/denny1.jpg" alt="" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+        <span class="brand-text font-weight-light">Kawal Corona </span>
+      </a>
+      
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
---
+++ application/views/layout/v_wrapper.php
@@ -0,0 +1,8 @@
+<?php 
+
+ $this->load->view('layout/v_head');
+ $this->load->view('layout/v_header');
+ $this->load->view('layout/v_nav');
+ $this->load->view('layout/v_content');
+ $this->load->view('layout/v_footer');
+
